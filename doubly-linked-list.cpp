5f151b6d8aed9821f569a65dd023a84599726d79
Vilayat <vilayatk96@outlook.com>
2024-01-22 02:43:52 +0530

    Implement inserting node at position in doubly linked list

--- doubly-linked-list.cpp
+++ doubly-linked-list.cpp
@@ -30,6 +30,34 @@ void TraverseList(ListNode *head)
         std::cout << "prev: " << current->_prev << "; Value: " << current->_value << "; next: " << current->_next <<std::endl;
         current = current->_next;
     }
+    std::cout << std::endl << std::endl;
+}
+
+void InsertNodeAt(ListNode*& head, int position, int value)
+{
+    if (position == 0)
+    {
+        ListNode* new_node = new ListNode(value);
+        new_node->_next = head;
+        head->_prev = new_node;
+        head = new_node;
+        return;
+    }
+    ListNode* current = head;
+    int counter = 0;
+
+    while (counter < position) {
+        current = current->_next;
+        counter++;
+    }
+    if (counter == position) {
+        ListNode* new_node = new ListNode(value);
+        new_node->_prev = current->_prev;
+        new_node->_next = current;
+
+        current->_prev->_next = new_node;
+        current->_prev = new_node;
+    }
 }
 
 int main()
@@ -37,6 +65,8 @@ int main()
     const int length = 5;
     ListNode *head = new ListNode(length);
     CreateList(head, length);
+    TraverseList(head);
+    InsertNodeAt(head, 2, 500);
     TraverseList(head);
     return 0;
 }
